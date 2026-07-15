e54f1f20a194e4d616d18d146e1051d829d20fb4
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-07-15 20:23:47 +0530

    Set up application routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import Navbar from "./components/Navbar"
+import Work from "./pages/Work"
+import About from "./pages/About"
+import Playground from "./pages/Playground"
+import Resource from "./pages/Resource"
+
+import {Routes, Route} from "react-router-dom"
 const App = () => {
   return (
     <>
     <Navbar/> 
+    <Routes>
+      <Route path= '/' element={<Work />}/>
+      <Route path= '/about' element={<About />}/>
+      <Route path= '/playground' element={<Playground />}/>
+      <Route path= '/resource' element={<Resource />}/>
+    </Routes>
     </>
   )
 }
