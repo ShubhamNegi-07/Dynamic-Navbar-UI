23d3ca48ad7938c748968a4f001f350660aaed73
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-07-15 20:23:08 +0530

    Implement responsive mobile menu with toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import { NavLink } from 'react-router-dom';
 import Logo from "../assets/logo.png"
 
 const Navbar = () => {
@@ -17,8 +18,8 @@ const Navbar = () => {
     },[]);
 
   return (
-    <header classname='m-top-80 bg-red-700 shadow-md'>
-     <div className='max-w-7xl bg-black  mx-auto px-5 rounded-full'>
+    <header className='m-10'>
+     <div className='max-w-7xl bg-black text-white mx-auto px-5 rounded-full'>
       <div className='h-20 flex justify-between items-center'>
 
       {/*logo*/}
@@ -26,10 +27,10 @@ const Navbar = () => {
 
       {/*desktop navlinks*/}
       <nav className='hidden md:flex gap-8'>
-        <a href="/" className='text-gray-700 hover:text-blue-600'>Work</a>
-        <a href="/about" className='text-gray-700 hover:text-blue-600'>About</a>
-        <a href="/playground" className='text-gray-700 hover:text-blue-600'>Playground</a>
-        <a href="/resource" className='text-gray-700 hover:text-blue-600'>Resource</a>
+        <NavLink to="/">Work</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/playground">Playground</NavLink>
+        <NavLink to="/resource">Resource</NavLink>
       </nav>
 
       {/* button */}
@@ -39,18 +40,18 @@ const Navbar = () => {
 
        {/* Hamburger menu */}
 
-       <button className='text-white md:hidden text-3xl' onClick={()=>setIsOpen(!isOpen)}>
+       <button className='bg-black md:hidden text-3xl' onClick={()=>setIsOpen(!isOpen)}>
         {isOpen ? "✕" : "☰" }
        </button>
       </div>
 
       {/*Mobile menu*/}
       {isOpen && (
-        <div className='md:hidden bg-white border-t'>
-            <a href="/" className='block px-6 py-4 hover:bg-gray-100'>Work</a>
-            <a href="/about" className='block px-6 py-4 hover:bg-gray-100'>About</a>
-            <a href="/playground" className='block px-6 py-4 hover:bg-gray-100'>Playground</a>
-            <a href="/resource" className='block px-6 py-4 hover:bg-gray-100'>Resource</a>
+        <div className='md:hidden text-white border-t'>
+            <NavLink to="/" className='block px-6 py-4 hover:bg-gray-100'>Work</NavLink>
+            <NavLink to="/about" className='block px-6 py-4 hover:bg-gray-100'>About</NavLink>
+            <NavLink to="/playground" className='block px-6 py-4 hover:bg-gray-100'>Playground</NavLink>
+            <NavLink to="/resource" className='block px-6 py-4 hover:bg-gray-100'>Resource</NavLink>
             <button className='md:hidden md:block bg-white w-full mb-4 hover:bg-blue-500 text-black px-6 py-2 rounded-full cursor-pointer'>
                 Login
             </button>
